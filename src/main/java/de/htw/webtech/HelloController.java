8cd172584a63c79002ffedcd0f0fc7e5cd0ae899
justinkyz <lejustin131@gmail.com>
2024-06-04 16:40:06 +0200

    Leerzeichen

--- src/main/java/de/htw/webtech/HelloController.java
+++ src/main/java/de/htw/webtech/HelloController.java
@@ -12,6 +12,7 @@ public class HelloController {
     @Autowired
     private StrategyService strategyService;
 
+
     @GetMapping
     public List<Strategy> getAllStrategies() {
         return strategyService.getAllStrategies();
