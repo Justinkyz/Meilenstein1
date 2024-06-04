12a1462eb985358061cad62b03980e5183fa0766
justinkyz <lejustin131@gmail.com>
2024-06-04 16:45:44 +0200

    adds cors

--- src/main/java/de/htw/webtech/HelloController.java
+++ src/main/java/de/htw/webtech/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/strategies")
+@CrossOrigin(origins = {"https://blackjack-tent.onrender.com"})
 public class HelloController {
 
     @Autowired
