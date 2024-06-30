abc1b9543cd3f94486b6407ddc1ecb9638db0b9f
justinkyz <lejustin131@gmail.com>
2024-06-30 15:55:43 +0200

    funktionier endlich

--- src/main/java/de/htw/webtech/DataController.java
+++ src/main/java/de/htw/webtech/DataController.java
@@ -3,11 +3,16 @@ package de.htw.webtech;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class DataController {
 
     @GetMapping("/api/data")
-    public String getData() {
-        return "Hier sind die Daten!";
+    public Map<String, String> getData() {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Hier sind die Daten!");
+        return response;
     }
 }
