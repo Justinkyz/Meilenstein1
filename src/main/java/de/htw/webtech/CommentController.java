710dd53eb1f33a91038eb1b57839d75af0e3b89b
justinkyz <lejustin131@gmail.com>
2024-06-30 17:20:50 +0200

    Kommentarfunktion + bindung hoffentlich mit Datenbak

---
+++ src/main/java/de/htw/webtech/CommentController.java
@@ -0,0 +1,24 @@
+package de.htw.webtech;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/comments")
+public class CommentController {
+
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @GetMapping
+    public List<Comment> getComments() {
+        return commentRepository.findAll();
+    }
+
+    @PostMapping
+    public Comment addComment(@RequestBody Comment comment) {
+        return commentRepository.save(comment);
+    }
+}
--- src/main/java/de/htw/webtech/DataController.java
+++ src/main/java/de/htw/webtech/DataController.java
@@ -3,16 +3,11 @@ package de.htw.webtech;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @RestController
 public class DataController {
 
     @GetMapping("/api/data")
-    public Map<String, String> getData() {
-        Map<String, String> response = new HashMap<>();
-        response.put("message", "Hier sind die Daten!");
-        return response;
+    public String getData() {
+        return "Hier sind die Daten!";
     }
 }
