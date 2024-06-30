437c9b44f6bc5d8b1d2259ccfe300b2f8a399fe3
justinkyz <lejustin131@gmail.com>
2024-06-30 18:39:05 +0200

    Login plus Datenbank

---
+++ src/main/java/de/htw/webtech/LoginController.java
@@ -0,0 +1,23 @@
+package de.htw.webtech;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/api/login")
+    public String login(@RequestBody User user) {
+        User foundUser = userRepository.findByUsername(user.getUsername());
+        if (foundUser != null && foundUser.getPassword().equals(user.getPassword())) {
+            return "Login successful";
+        } else {
+            return "Invalid username or password";
+        }
+    }
+}
