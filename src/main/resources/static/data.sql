b8e1faad84ed7570f16efc68ab817c992803a0c5
justinkyz <lejustin131@gmail.com>
2024-07-02 01:31:08 +0200

    02.07.24 1:31

--- src/main/resources/static/data.sql
+++ src/main/resources/static/data.sql
@@ -1,2 +1,8 @@
+CREATE TABLE IF NOT EXISTS app_user (
+                                        id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                                        username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+    );
+
 INSERT INTO app_user (username, password) VALUES ('Justin', 'Justin1301');
 INSERT INTO app_user (username, password) VALUES ('user2', 'password2');
